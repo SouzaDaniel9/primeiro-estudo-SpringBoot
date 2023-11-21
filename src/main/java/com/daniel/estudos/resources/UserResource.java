7230199865cf994c440cf4a1ee73e95965e4af33
Daniel Souza <Danielflorindo9@gmail.comgit config --global user.email Danielflorindo9@gmail.com>
2023-11-21 09:22:23 -0300

    criado a classe user

---
+++ src/main/java/com/daniel/estudos/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.daniel.estudos.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.daniel.estudos.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Daniel", "daniel@gmail.com", "864864684", "123");
+		return ResponseEntity.ok().body(u);
+	}
+}
