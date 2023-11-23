1ada7ce1db10fad4d12179851865c66e6a2699b9
Daniel Souza <Danielflorindo9@gmail.comgit config --global user.email Danielflorindo9@gmail.com>
2023-11-23 20:21:31 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/daniel/estudos/entities/Product.java
+++ src/main/java/com/daniel/estudos/entities/Product.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
@@ -35,8 +36,15 @@ public class Product implements Serializable {
 	private Double price;
 	private String imgUrl;
 
-	public Product(Long id, String name, String description, Double price, String imgUrl) {
+	@Setter(value = AccessLevel.NONE)
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
 
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		this.id = id;
 		this.name = name;
 		this.description = description;
@@ -44,8 +52,12 @@ public class Product implements Serializable {
 		this.imgUrl = imgUrl;
 	}
 
-	@Setter(value = AccessLevel.NONE)
-	@ManyToMany
-	@JoinTable(name = "tb_product_category",  joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
-	private Set<Category> categories = new HashSet<>();
+//	@JsonIgnore
+	public Set<Order> getOrders() {
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
+	}
 }
